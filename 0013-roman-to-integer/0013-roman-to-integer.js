c8834b0ca34fe4006e0a887466e414dfcef2c5e0
akio <124953279+akiomatic108@users.noreply.github.com>
2023-09-22 23:24:32 -0700

    Time: 121 ms (24.58%), Space: 46.8 MB (74.82%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -5,64 +5,40 @@
 var romanToInt = function (s) {
   let output = 0;
   const arr = [...s];
-  let index = 0;
-  const irregulars = ["I", "X", "C"];
-  const irregularsNext = [
-    ["V", "X"],
-    ["L", "C"],
-    ["D", "M"],
-  ];
-  let irrIndex;
-  let irrNextIndex;
   let nextChar;
 
-  while (index < arr.length) {
-    irrIndex = irregulars.findIndex((e) => e === arr[index]);
+  for (let i = 0; i < arr.length; i++) {
+      nextChar = arr[i + 1];
 
-    if (index + 1 < arr.length && irrIndex !== -1) {
-      nextChar = arr[index + 1];
-      irrNextIndex = irregularsNext[irrIndex].findIndex((e) => e === nextChar);
-
-      if (irrNextIndex !== -1) {
-        if (irrIndex === 0) {
-          if (irrNextIndex === 0) {
-            output += 4;
-          } else {
-            output += 9;
-          }
-        } else if (irrIndex === 1) {
-          if (irrNextIndex === 0) {
-            output += 40;
-          } else {
-            output += 90;
-          }
-        } else {
-          if (irrNextIndex === 0) {
-            output += 400;
-          } else {
-            output += 900;
-          }
-        }
-
-        index += 2;
-        continue;
-      }
-    }
-
-    switch (arr[index]) {
+      switch (arr[i]) {
       case "I":
+        if (nextChar === "V" || nextChar === "X") {
+            output += nextChar === "V" ? 4 : 9;
+            i++;
+            continue;
+        }
         output += 1;
         break;
       case "V":
         output += 5;
         break;
       case "X":
+        if (nextChar === "L" || nextChar === "C") {
+            output += nextChar === "L" ? 40 : 90;
+            i++;
+            continue;
+        }
         output += 10;
         break;
       case "L":
         output += 50;
         break;
       case "C":
+        if (nextChar === "D" || nextChar === "M") {
+            output += nextChar === "D" ? 400 : 900;
+            i++;
+            continue;
+        }
         output += 100;
         break;
       case "D":
@@ -71,8 +47,6 @@ var romanToInt = function (s) {
       default:
         output += 1000;
     }
-
-    index++;
   }
 
   return output;
