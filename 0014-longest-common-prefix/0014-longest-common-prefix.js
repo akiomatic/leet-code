da66e7e6c7051fa79939f9ca589d596473a8cf40
akio <124953279+akiomatic108@users.noreply.github.com>
2023-09-23 20:10:49 -0700

    Time: 60 ms (30.62%), Space: 43.9 MB (16.29%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -5,10 +5,12 @@
 var longestCommonPrefix = function(strs) {
     let prefix = "";
     const first = strs[0];
-    for (let i = 0; i < first.length; i++) {
-        if (strs.every(e => e.charAt(i) === first[i])) {
+
+    for (const i in first) {
+        if (strs.filter(e => e.startsWith(prefix + first[i])).length === strs.length) {
             prefix += first[i];
         } else return prefix;
     }
+
     return prefix;
 };
